9f1bed3c3a5c52866a35cd030892a787df2626dc
Lloyd Encallado <lloydencallado@gmail.com>
2024-09-07 01:15:00 +0800

    added mdi/font

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import '@mdi/font/css/materialdesignicons.css'
 import { createApp } from 'vue';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components'
